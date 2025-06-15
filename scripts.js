7113882dcaf49491f293fb0af2094f50b776e4ad
clovis-cordeiro-morici <illusion350@hotmail.com>
2025-06-14 22:03:57 -0300

    Atualização index.html, styles.css e scripts.js do Convert Money

--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
-const button = document.querySelector(".convert-button")
+const button1 = document.querySelector(".convert-button")
 
 function convertvalue() {
-    const inputcurrencyvalue=document.querySelector(".currency-value").value
-    const dolartoday= 5
-    const valueconverted= inputcurrencyvalue/dolartoday
-    console.log(valueconverted)
+    
+    const inputcurrencyvalue = document.querySelector(".currency-value").value
+    const valuetoconvert = document.querySelector(".valuereal1")
+    const valueconverted = document.querySelector(".valuedolar1")
+    const dolartoday= 5.2
+    
+    const resultconverted = inputcurrencyvalue / dolartoday
+
+    valuetoconvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency:"BRL"
+    }). format(inputcurrencyvalue)
+
+   valueconverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency:"USD"
+    }). format(resultconverted )
+   
+
+   
+    
 }
 
-button.addEventListener("click", convertvalue)
+button1.addEventListener("click", convertvalue)
